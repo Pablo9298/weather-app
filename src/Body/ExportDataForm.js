a98288b8500f2fd898d37b9bb7d48246bf99a207
Pablo9298 <wmpawka@gmail.com>
2022-12-27 15:17:54 +0200

    exportDataForm

--- src/Body/ExportDataForm.js
+++ src/Body/ExportDataForm.js
@@ -2,7 +2,7 @@ import FormGroup from 'react-bootstrap/FormGroup';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-function ExportDataForm(defaultProps) {
+function ExportDataForm() {
 
   const modes = ["JSON", "XML", "HTML"];
 
@@ -10,22 +10,25 @@ function ExportDataForm(defaultProps) {
     event.preventDefault();
     console.log(event);
 
+    // const mode = event.target.mode.value;
+    // window.open().document.write(`Your choice data format ${mode}`)
+
     window.open().document.write(`Your choice data format ${event.target.mode.value}`);
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <FormGroup className="my-4">
-        <Form.Label>Modes</Form.Label>
-        <Form.Select name="mode" defaultValue={defaultProps.mode}>
+        <h5 className='mb-5'>Export</h5>
+        <Form.Select name="mode" defaultValue="JSON">
           {modes.map((mode) => (
             <option key={mode} value={mode}>{mode}</option>
           ))}
         </Form.Select>
       </FormGroup>
 
-      <Button className="w-100" variant="primary" type="submit">
-        Submit
+      <Button className="w-100" variant="warning" type="submit">
+        Export
       </Button>
     </Form >
   );
