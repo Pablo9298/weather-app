fe8c079f181ba900a3743e8712b06f6a7929da1e
Pablo9298 <wmpawka@gmail.com>
2022-12-29 18:08:01 +0200

    changedErrorCatchForecast

--- src/Body/Tabs/Forecast.js
+++ src/Body/Tabs/Forecast.js
@@ -1,28 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import TimeSelector from "./TimeSelector";
 import Map from "./Map";
 import { getForecast } from "../../services/apiService";
-import ErrorModal from "../../ErrorModal";
 
 function Forecast() {
 
-  const [errorMessage, setErrorMessage] = useState(null);
-
   useEffect(() => {
     (async function () {
-      try {
-        const weather = await getForecast();
-        const response = await weather.json();
-
-        console.log('response', response);
-
-        if (response.cod !== 200) {
-          throw Error(response.message);
-        }
-      } catch (error) {
-        console.log(error);
-        setErrorMessage(error.message);
-      }
+      const weather = await getForecast();
+      const response = await weather.json();
+      console.log('response', response);
     })()
   }, []);
 
@@ -30,7 +17,6 @@ function Forecast() {
     <>
       <TimeSelector id="forcast" />
       <Map />
-      <ErrorModal message={errorMessage} handleClose={() => setErrorMessage(null)} />
     </>
   );
 }
