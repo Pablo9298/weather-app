7d123cbd30d2e568e24418f2d9de51cc1aaeb5c6
Pablo9298 <wmpawka@gmail.com>
2023-01-19 21:42:37 +0200

    reduxError

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Body from './Body';
 import Header from './Header';
+import ErrorModal from './ErrorModal';
 
 function App() {
   return (
     <Container>
       <Header />
+      <ErrorModal />
       <Body />
     </Container>
   );
--- src/Body/Tabs/Now.js
+++ src/Body/Tabs/Now.js
@@ -3,40 +3,46 @@ import Map from "./Map";
 import { getWeather } from "../../services/apiService";
 import ErrorModal from "../../ErrorModal";
 import Data from "./Data";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setErrorMessage } from '../../services/stateService';
 
 function Now() {
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [weatherData, setWeatherData] = useState(null);
+    // const [errorMessage, setErrorMessage] = useState(null);
+    const [weatherData, setWeatherData] = useState(null);
 
-  const searchParams = useSelector((state) => state.searchParams);
+    const searchParams = useSelector((state) => state.searchParams);
+    const errorMessage = useSelector(state => state.errorMessage);
 
-  useEffect(() => {
-    (async function () {
-      try {
-        const response = await getWeather(searchParams);
-        const data = await response.json();
+    const handleClose = () => dispatch(setErrorMessage(false));
 
-        if (data.cod !== 200) throw Error(data.message);
+    const dispatch = useDispatch();
 
-        setWeatherData(data);
-      } catch (error) {
-        console.log(error);
-        setErrorMessage(error.message);
-      }
-    })();
-  }, [searchParams]);
+    useEffect(() => {
+      (async function () {
+        try {
+          const response = await getWeather(searchParams);
+          const data = await response.json();
 
-  return (
-    <>
-      <Data data={weatherData} />
-      <Map weatherData={weatherData} />
-      <ErrorModal
-        message={errorMessage}
-        handleClose={() => setErrorMessage(null)}
-      />
-    </>
-  );
-}
+          if (data.cod !== 200) throw Error(data.message);
+
+          setWeatherData(data);
+        } catch (error) {
+          console.log(error);
+          dispatch(setErrorMessage(error.message));
+        }
+      })();
+    }, [searchParams, dispatch]);
+
+    return (
+      <>
+        <Data data={weatherData} />
+        <Map weatherData={weatherData} />
+        <ErrorModal
+          message={errorMessage}
+          handleClose={handleClose}
+        />
+      </>
+    );
+  }
 
 export default Now;
